3180ce9c7fc96f0ff73b2c1ea4b961427242ed95
RoshaniBarochia <133959425+RoshaniBarochia@users.noreply.github.com>
2023-05-18 16:08:56 +0530

    Update GenericKotlinDemo.kt
    add function and class changes

--- app/GenericKotlinDemo.kt
+++ app/GenericKotlinDemo.kt
@@ -1,7 +1,9 @@
-import java.util.Arrays
+import java.util.*
 
-class GenericKotlinDemo {
-}
+/**
+ * Generic Class demo*/
+class GenericKotlinDemo<T>(val item: T)
+
 
 /**
  * Generic function demo
@@ -11,12 +13,24 @@ fun <T : Comparable<T>> sortArray(array: Array<T>) {
     Arrays.sort(array)
 }
 fun main(){
-    /** pass integer datatype*/
+
+    /** for class in generic type*/
+    val box1: GenericKotlinDemo<Int> = GenericKotlinDemo(42)
+    val box2: GenericKotlinDemo<String> = GenericKotlinDemo("Hello, world!")
+    println("Integer : ${box1.item}")
+    println("String : ${box2.item}")
+
+    /** for function in generic type pass integer & string datatype */
     val array = arrayOf(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5)
     sortArray(array)
-
-    /** pass String datatype*/
+    for (element in array) {
+        println("Integer Array output : $element")
+    }
     val arrayStr = arrayOf("apple", "banana", "cherry", "date", "elderberry")
     sortArray(arrayStr)
+    for (element in arrayStr) {
+        println("String Array output : $element")
+    }
 
 }
+
