6d48c99767bdeb3b8fe2417982d17d1e72ce8a1a
RoshaniBarochia <133959425+RoshaniBarochia@users.noreply.github.com>
2023-05-20 14:28:13 +0530

    Update GenericKotlinDemo.kt

--- app/GenericKotlinDemo.kt
+++ app/GenericKotlinDemo.kt
@@ -7,27 +7,27 @@ class GenericKotlinDemo<T>(val item: T)
 
 /**
  * Generic function demo
- * create generic function sortArray
+ * create generic function sortList
  * pass and return any data type of array*/
-fun <T : Comparable<T>> sortArray(array: Array<T>) {
-    Arrays.sort(array)
+fun <T : Comparable<T>> sortList(list: Array<T>) {
+    Arrays.sort(list)
 }
 fun main(){
 
     /** for class in generic type*/
-    val box1: GenericKotlinDemo<Int> = GenericKotlinDemo(42)
-    val box2: GenericKotlinDemo<String> = GenericKotlinDemo("Hello, world!")
-    println("Integer : ${box1.item}")
-    println("String : ${box2.item}")
+    val demo1: GenericKotlinDemo<Int> = GenericKotlinDemo(42)
+    val demo2: GenericKotlinDemo<String> = GenericKotlinDemo("Hello, world!")
+    println("Integer : ${demo1.item}")
+    println("String : ${demo2.item}")
 
     /** for function in generic type pass integer & string datatype */
-    val array = arrayOf(3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5)
-    sortArray(array)
+    val array = arrayOf(31, 11, 14, 11, 15, 91, 53, 52, 6,1)
+    sortList(array)
     for (element in array) {
         println("Integer Array output : $element")
     }
-    val arrayStr = arrayOf("apple", "banana", "cherry", "date", "elderberry")
-    sortArray(arrayStr)
+    val arrayStr = arrayOf("dairy milk", "kitkat", "eclairs", "five star")
+    sortList(arrayStr)
     for (element in arrayStr) {
         println("String Array output : $element")
     }
